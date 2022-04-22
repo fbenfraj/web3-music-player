b3396fde8a09391c1a7fe893bf29131da0eb0b59
fbenfraj <farouk.ben-fraj@outlook.com>
2022-04-22 22:24:12 +0100

    Fix contract declaration and hardhat deployment script

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -1,15 +1,19 @@
 async function main() {
-
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   // deploy contracts here:
-  
-  
+  const NFTMarketplaceFactory = await ethers.getContractFactory(
+    "MusicNFTMarketplace"
+  );
+  const nftMarketplace = await NFTMarketplaceFactory.deploy();
+
+  console.log("Smart contract address: ", nftMarketplace.address);
+
   // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
-  saveFrontendFiles();
+  saveFrontendFiles(nftMarketplace, "NFTMarketplaceFactory");
 }
 
 function saveFrontendFiles(contract, name) {
@@ -35,7 +39,7 @@ function saveFrontendFiles(contract, name) {
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
--- src/contracts/MusicNFTMarketplace.sol
+++ src/contracts/MusicNFTMarketplace.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MusicNFTMarketplace is ERC721, Ownable {
+contract MusicNFTMarketplace is
+    ERC721("MusicNFTMarketplace", "MNFTM"),
+    Ownable
+{
     // Audio files can be uploaded on https://nft.storage/
     // CAR file obtainable on https://car.ipfs.io/
     string public baseURI =
